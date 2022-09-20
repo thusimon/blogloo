eb1089977e0e3f57b10f253f6b7346ffc6809a2b
Lu Xu <thusimon@gmail.com>
2022-09-19 21:46:53 -0700

    add spring security config

---
+++ src/main/java/com/utticus/blogloo/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.utticus.blogloo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @PostMapping("/")
+    public boolean auth() {
+        return true;
+    }
+}
---
+++ src/main/java/com/utticus/blogloo/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.utticus.blogloo.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Value("${spring.security.debug:false}")
+    boolean securityDebug;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/**")
+                .hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/**")
+                .hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/**")
+                .hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/**")
+                .permitAll()
+                .antMatchers("/", "/static/**", "/manifest.json", "/*.png")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.debug(securityDebug)
+                .ignoring()
+                .antMatchers("/static/**", "/favicon.ico", "/manifest.json");
+    }
+}
