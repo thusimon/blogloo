dd6fd879b23c00e80f1abebae9a34e9c72cdab37
Lu Xu <thusimon@gmail.com>
2022-10-27 18:00:56 -0700

    fix autowired null

--- src/main/java/com/utticus/blogloo/config/RedisConfig.java
+++ src/main/java/com/utticus/blogloo/config/RedisConfig.java
@@ -3,6 +3,12 @@ package com.utticus.blogloo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -10,11 +16,29 @@ import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
+//    @Bean
+//    JedisConnectionFactory jedisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setHostName("localhost");
+//        redisStandaloneConfiguration.setPort(6379);
+//        redisStandaloneConfiguration.setDatabase(0);
+//        redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
+//
+//        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+//        jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60));// 60s connection timeout
+//
+//        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(
+//                redisStandaloneConfiguration,
+//                jedisClientConfiguration.build()
+//        );
+//
+//        return jedisConFactory;
+//    }
+
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofDays(2)) // Changing the cache TTL
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
--- src/main/java/com/utticus/blogloo/config/WebMvcConfig.java
+++ src/main/java/com/utticus/blogloo/config/WebMvcConfig.java
@@ -1,14 +1,18 @@
 package com.utticus.blogloo.config;
 
 import com.utticus.blogloo.filter.IPRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    IPRequestFilter ipRequestFilter;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new IPRequestFilter()).addPathPatterns("/**");
+        registry.addInterceptor(ipRequestFilter).addPathPatterns("/**");
     }
 }
