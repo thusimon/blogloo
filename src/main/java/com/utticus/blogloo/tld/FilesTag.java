249463e6493014f702a36283e42b19958cdc4777
Lu Xu <thusimon@gmail.com>
2022-12-02 21:39:38 -0800

    add webpack assets to cache

--- src/main/java/com/utticus/blogloo/tld/FilesTag.java
+++ src/main/java/com/utticus/blogloo/tld/FilesTag.java
@@ -3,8 +3,6 @@ package com.utticus.blogloo.tld;
 import com.google.gson.Gson;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,22 +12,28 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FilesTag{
+public class FilesTag {
     private static final Logger logger = LogManager.getLogger(FilesTag.class);
 
     private static final String WEBPACK_ASSETS_MANIFEST_PATH = "assets-manifest.json";
 
-    @Autowired
-    RedisTemplate<String, Object> redisTemplate;
+    private static Map<String, Map<String, String>> fileResourceCache = new HashMap<>();
+
     public static Map<String, String> getManifestJson(String fileName) {
         try {
+            Map<String, String> manifestMap = fileResourceCache.get(fileName);
+            if (manifestMap != null && !manifestMap.isEmpty()) {
+                return manifestMap;
+            }
+            logger.info("manifest file {} has not been cached, retrieving from file and adding to cache", fileName);
             File manifestFile = new File(fileName);
             Reader reader = new FileReader(manifestFile);
-            HashMap<String, String> resourceFileMap = new Gson().fromJson(reader, HashMap.class);
-            if (resourceFileMap.isEmpty()) {
+            manifestMap = new Gson().fromJson(reader, HashMap.class);
+            if (manifestMap.isEmpty()) {
                 logger.error("Error retrieving data in {}", fileName);
             }
-            return resourceFileMap;
+            fileResourceCache.put(fileName, manifestMap);
+            return manifestMap;
         } catch (FileNotFoundException e) {
             logger.error("File {} not found", fileName, e);
         } catch (Exception e) {
@@ -40,29 +44,9 @@ public class FilesTag{
 
     public static String getManifestResource(String resource) {
         try {
-            // Load the webpack.manifest.json file from classpath.
             ClassLoader classLoader =  FilesTag.class.getClassLoader();
             File manifestFile = new File(classLoader.getResource(WEBPACK_ASSETS_MANIFEST_PATH).getFile());
-            //String cacheKey = createCacheKey(Long.toString(manifestFile.lastModified()), "webpackmanifest");
-            Map<String, String> resourceFileMap = Collections.EMPTY_MAP;
-
-            logger.debug("Manifest has not been cached, retrieving JSON data and adding to cache");
-            resourceFileMap = getManifestJson(manifestFile.getPath());
-
-            /*
-             * Retrieve resourceFileMap values if it has already been cached
-             * with the lastModDate as the cache key. If it doesn't exist in the
-             * event that manifest has been updated, cache the new manifest with
-             * new key/values.
-             */
-//            if (CustomCache.get(cacheKey) != null) {
-//                log.debug("Manifest has already been cached, returning cached value");
-//                resourceFileMap = (Map<String, String>) CustomCache.get(cacheKey);
-//            } else {
-//                log.debug("Manifest has not been cached, retrieving JSON data and adding to cache");
-//                resourceFileMap = getManifestJson(manifestFile.getPath());
-//                CustomCache.cache(cacheKey, resourceFileMap, TWELVE_HOUR_CACHE_DURATION);
-//            }
+            Map<String, String> resourceFileMap = getManifestJson(manifestFile.getPath());
             return resourceFileMap.get(resource);
         } catch (Exception e) {
             logger.error("Error retrieving asset {} from manifest json resource map", resource, e);
