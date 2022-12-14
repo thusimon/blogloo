e103ce05b0f492504b1e0f1974ca82057fa72a34
Lu Xu <thusimon@gmail.com>
2022-12-13 21:54:58 -0800

    add preference modal

--- web-client/src/components/Head.tsx
+++ web-client/src/components/Head.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useAppContext, Actions } from '../context/app-context';
 import { LOCALE } from '../types';
 import { localeMapping } from './ArticleInfo';
+import Preference from './Preference';
 
 import './Head.scss';
 export interface HeaderPropType {
@@ -21,27 +22,35 @@ const Head = ({ showLocale }: HeaderPropType): JSX.Element => {
     dispatch({ type: Actions.UpdateLocale, data: { locale } });
   };
 
+  const onPreferenceClick = (): void => {
+    dispatch({ type: Actions.TogglePrefModal, data: {} });
+  };
+
   const getLocalesFromArticles = (): JSX.Element[] => {
     const locales = Object.keys(LOCALE) as LOCALE[];
     const filteredLocales = locales.filter(locale => state.listArticles.find(article => locale === article.locale));
     return filteredLocales.map(locale => {
       const { img, name } = localeMapping[locale];
       return <img src={img} title={name} alt={name} key={locale} data-se={locale}
-        className={locale === state.locale ? 'icon selected' : 'icon'}
+        className={`h-3/5 mx-[4px] cursor-pointer transition-transform duration-500 hover:scale-125 ${locale === state.locale ? 'selected' : ''}`}
         onClick={evt => { evt.stopPropagation(); updateLocale(locale); }}></img>;
     });
   };
 
-  return <header className='app-header'>
-    <span className='logo-text'>Blogloo</span>
-    <span>{t('preference')}</span>
+  return <header className='h-[30px] flex items-center justify-between bg-neutral-800 text-slate-100 pl-[8px] pr-[4px]'>
+    <div>
+      <span className='font-bold'>Blogloo</span>
+      <span className='border-r-2 mx-[12px]'></span>
+      <button className='mr-[12px] hover:text-white' onClick={onPreferenceClick}>{t('preference')}</button>
+    </div>
     {
-      showLocale && <div className='locale-select'>
+      showLocale && <div className='h-full flex items-center'>
         {
           getLocalesFromArticles()
         }
       </div>
     }
+    <Preference />
   </header>;
 };
 
